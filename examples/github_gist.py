7654be26bb872efb554eb4c28b80aeca088e550c
Jacob Burch <jacobburch@gmail.com>
2012-07-27 03:43:26 -0500

    add github gist example

---
+++ examples/github_gist.py
@@ -0,0 +1,42 @@
+import json
+
+import nap
+
+
+class GistFile(nap.ResourceModel):
+    size = nap.Field(readonly=True)
+    # filename = nap.Field()
+    raw_url = nap.Field(readonly=True)
+    content = nap.Field()
+
+    class Meta:
+        root_url = 'https://api.github.com/gists'
+
+
+class Gist(nap.ResourceModel):
+    url = nap.Field(readonly=True)
+    id = nap.Field(resource_id=True)
+    description = nap.Field()
+    public = nap.Field()
+    user = nap.Field()
+    html_url = nap.Field(readonly=True)
+    files = nap.DictField(GistFile)
+
+    def handle_create_response(self, r):
+        try:
+            response_data = json.loads(r.content)
+        except ValueError:
+            return
+
+        self.update_fields(response_data)
+
+    class Meta:
+        resource_name = 'gists'
+        root_url = 'https://api.github.com/'
+        add_slash = False
+
+
+# gf = GistFile(content='test')
+# g = Gist(description="nap test", files={'what.txt': gf}, public=True)
+# g.save()
+# g.html_url
