c88399732e8b87f1cf85d15f4f6d8254d1de258b
Jacob Burch <jacobburch@gmail.com>
2012-07-27 02:58:23 -0500

    clean up add_slash, add tests

--- nap/utils.py
+++ nap/utils.py
@@ -1,10 +1,12 @@
 import urllib
 
 
-def make_url(base_url, params=None, add_slash=True):
+def make_url(base_url, params=None, add_slash=None):
     "Split off in case we need to handle more scrubing"
     if add_slash and not base_url.endswith('/'):
         base_url = "%s/" % base_url
+    elif add_slash is False and base_url.endswith('/'):
+        base_url = base_url[:-1]
 
     if params:
         param_string = urllib.urlencode(params)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,10 +2,22 @@ from nap.utils import make_url
 
 
 def test_url():
-    url = make_url("http://www.google.com", params={'x': '1'})
+    url = make_url("http://www.google.com/", params={'x': '1'})
     assert url == "http://www.google.com/?x=1"
 
 
 def test_no_params():
-    url = make_url("http://www.google.com")
+    url = make_url("http://www.google.com/")
     assert url == "http://www.google.com/"
+
+
+def test_add_slash():
+    url = "http://www.google.com"
+    assert make_url(url, add_slash=True) == "http://www.google.com/"
+    assert make_url(url, add_slash=False) == "http://www.google.com"
+    assert make_url(url, params={'x': 1}) == "http://www.google.com?x=1"
+
+    url_s = "http://www.google.com/"
+    assert make_url(url_s, add_slash=True) == "http://www.google.com/"
+    assert make_url(url_s, add_slash=False) == "http://www.google.com"
+    assert make_url(url_s, params={'x': 1}) == "http://www.google.com/?x=1"
