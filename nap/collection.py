5b8d2f451358d6e343a26d2e59b46c74c1f414a6
Jacob Burch <jacobburch@gmail.com>
2013-08-20 13:32:04 -0500

    allow ListWithAttributes to take no extra_data

--- nap/collection.py
+++ nap/collection.py
@@ -1,7 +1,9 @@
 
 
 class ListWithAttributes(list):
-    def __init__(self, list_vals, extra_data):
-        super(ListWithAttributes, self).__init__()
-        self.extend(list_vals)
+    def __init__(self, list_vals, extra_data=None):
+
+        if not extra_data:
+            extra_data = {}
+        super(ListWithAttributes, self).__init__(list_vals)
         self.extra_data = extra_data
