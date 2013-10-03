7d743e3ae6468e8398a68d05bb5209c30b580df2
Jacob Burch <jacobburch@gmail.com>
2013-10-03 13:32:36 -0500

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 test_requirements = ['mock', ]
 setup(
     name='nap',
-    version="0.3.4",
+    version="0.3.5",
     description=('api access modeling and tools'),
     author="Jacob Burch",
     author_email="jacobburch@gmail.com",
